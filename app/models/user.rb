4fc220cf32a672a89daeb7dd243ddeb72eeb7ded
Giorgio Zanni <g.zanni@hotmail.com>
2019-02-01 17:03:25 -0300

    FIX: Delete dependency

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :playlists
+  has_many :playlists, dependent: :destroy
   validates :name, :email, presence: true
   validates :email, uniqueness: true
 end
